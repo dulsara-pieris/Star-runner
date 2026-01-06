546c5788cc03c4d0c27111e83f8c11721f3bee6e
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-01-06 17:34:42 +0530

    Game

--- game.sh
+++ game.sh
@@ -96,23 +96,26 @@ init_profile() {
     total_asteroids=0
 
     # Save initial profile
-    save_profile
-    printf "\n${COLOR_GREEN}Profile created successfully! Welcome, ${player_title} ${player_name} (Age: ${player_age})!${COLOR_NEUTRAL}\n\n"
-    sleep 2
-  else
-    load_profile
-    # Calculate current age from birth year
-    current_year=2026
-    player_age=$((current_year - player_birth_year))
-  fi
+save_profile() {
+  PROFILE_FILE="$SAVE_DIR/${player_name}.profile"
+  cat > "$PROFILE_FILE" << EOF
+  player_name="$player_name"
+  player_birth_year="$player_birth_year"
+  player_gender="$player_gender"
+  player_title="$player_title"
+  high_score=$high_score
+  total_crystals=$total_crystals
+  total_asteroids=$total_asteroids
+  EOF
 }
-
 load_profile() {
+  PROFILE_FILE="$SAVE_DIR/${player_name}.profile"
   if [ -f "$PROFILE_FILE" ]; then
     # shellcheck disable=SC1090
     . "$PROFILE_FILE"
   fi
 }
+
 
 
 # Constants
